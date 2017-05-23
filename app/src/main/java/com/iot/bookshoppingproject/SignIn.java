802d752609aa5182ff03a87c70fac5c1961b48ce
test2 <kgwug0723@gmail.com>
2017-05-23 14:38:40 +0900

    로그인과 사인인 그리고 Revise UI와 액티비티 추가했습니다.

--- app/src/main/java/com/iot/bookshoppingproject/SignIn.java
+++ app/src/main/java/com/iot/bookshoppingproject/SignIn.java
@@ -28,7 +28,6 @@ public class SignIn extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
         openDatabase();
