a952424aeae5d6de15e9e0ef15849ce4bc75abca
test2 <kgwug0723@gmail.com>
2017-05-24 11:43:04 +0900

    리사이클뷰 데이터베이스 생성 그리고 텍스트뷰에 프라이스 찍었습니다

--- app/src/main/java/com/iot/bookshoppingproject/BookListActivity.java
+++ app/src/main/java/com/iot/bookshoppingproject/BookListActivity.java
@@ -1,6 +1,9 @@
 package com.iot.bookshoppingproject;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -9,24 +12,29 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 import static com.iot.bookshoppingproject.R.id.UserText;
+import static java.sql.DriverManager.println;
 
 public class BookListActivity extends AppCompatActivity {
 
     ArrayList<RecycleData> recycleDataSet = new ArrayList<>();
-
+    private static String DATABASE_NAME = "store";
+    private static String TABLE_NAME = "books";
     TextView textViewTt;
     String userName;
-
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        openDatabase(DATABASE_NAME);
+        createTable(TABLE_NAME);
 
         textViewTt = (TextView) findViewById(UserText);
         Button button = (Button) findViewById(R.id.bt01);
@@ -57,7 +65,7 @@ public class BookListActivity extends AppCompatActivity {
 
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.Recyclerview);
 
-        InsertData(15);
+        InsertData(TABLE_NAME, 15);
 
         recyclerView.setAdapter(new RecyclerAdapter(recycleDataSet));
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -67,15 +75,51 @@ public class BookListActivity extends AppCompatActivity {
 
     }
 
-    public void InsertData(int i) {
-       for (int j = 0; j < i; j++) {
-            RecycleData recycleData = new RecycleData();
-            recycleData.setTextview_text("textView"+(j+1));
-           recycleDataSet.add(recycleData);
+    private void openDatabase(String DATABASE_NAME){
+        try{
+            db = openOrCreateDatabase(
+                    DATABASE_NAME,
+                    Activity.MODE_PRIVATE,
+                    null);
+        }catch (Exception e){
+            Toast.makeText(
+                    getApplicationContext(),
+                    "DB 오픈 실패",
+                    Toast.LENGTH_LONG
+            ).show();
+            e.printStackTrace();
+            e.getMessage().toString();
+        }
+    }
+
+    private void createTable(String TABLE_NAME){
+        try {
+            db.execSQL("create table if not exists " + TABLE_NAME +
+                    " ( " +
+                    " title text PRIMARY KEY, " +
+                    " price text, " +
+                    " barcode text);"
+            );
+        } catch (Exception e){
+            e.printStackTrace();
+            println(TABLE_NAME + " 테이블 생성 실패");
+            println((e.getMessage().toString()));
         }
     }
 
 
+    public void InsertData(String TABLE_NAME, int i) {
+       for (int j = 0; j < i; j++) {
+           String SQL = "select price " + " from " + TABLE_NAME;
+           Cursor cursor = db.rawQuery(SQL, null);
+//           int recordCount = cursor.getCount();
+           cursor.moveToNext();
+           String title = cursor.getString(0);
+           RecycleData recycleData = new RecycleData();
+           recycleData.setTextview_text(title);
+           recycleDataSet.add(recycleData);
+        }
+    }
 }
 
 
