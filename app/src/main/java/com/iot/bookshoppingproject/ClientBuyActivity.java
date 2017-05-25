93e41738a6dfd73fcb5e79906e26c330162c4fb1
chingline <chingline@naver.com>
2017-05-25 12:25:45 +0900

    팝업창 추가

--- app/src/main/java/com/iot/bookshoppingproject/ClientBuyActivity.java
+++ app/src/main/java/com/iot/bookshoppingproject/ClientBuyActivity.java
@@ -1,10 +1,7 @@
 package com.iot.bookshoppingproject;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -13,10 +10,6 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 /**
  * Created by CHJ on 2017-05-24.
@@ -28,10 +21,9 @@ public class ClientBuyActivity extends AppCompatActivity{
     TextView textViewTitle;
     TextView textViewPrice;
     String barcodeNumber;
-    String bookTitle;
-    int bookPrice;
     SQLiteDatabase db;
-    private Bitmap bitmap;
+
+    private PopupActivity mPopupActivity;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -41,16 +33,6 @@ public class ClientBuyActivity extends AppCompatActivity{
         Intent intent = getIntent();
         if(intent != null) {
             barcodeNumber = intent.getStringExtra("barcodeNumber");
-            bookTitle = intent.getStringExtra("booktitle");
-            bookPrice = intent.getIntExtra("bookprice", 0);
-        }
-
-        openDatabase("store");
-
-        try {
-            getBookimageThread(barcodeNumber);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
         }
 
         imageViewBook = (ImageView) findViewById(R.id.imageView);
@@ -59,68 +41,36 @@ public class ClientBuyActivity extends AppCompatActivity{
         Button buttonBuy = (Button) findViewById(R.id.button_buy);
         Button buttonExit = (Button) findViewById(R.id.button_exit);
 
-        imageViewBook.setImageBitmap(bitmap);
-        textViewTitle.setText(bookTitle);
-        textViewPrice.setText(bookPrice+" 원");
-
-        buttonExit.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        finish();
-                    }
-                }
-        );
-
-        buttonBuy.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-
-                    }
-                }
-        );
 
     }
 
-    private void openDatabase(String DATABASE_NAME){
-        try{
-            db = openOrCreateDatabase(
-                    DATABASE_NAME,
-                    Activity.MODE_PRIVATE,
-                    null);
-        }catch (Exception e){
-            Toast.makeText(
-                    getApplicationContext(),
-                    "DB 오픈 실패",
-                    Toast.LENGTH_LONG
-            ).show();
-            e.printStackTrace();
-            e.getMessage().toString();
+    public void onClickView(View v) {
+        switch (v.getId()) {
+            case R.id.button_buy:
+                mPopupActivity = new PopupActivity(this,
+                        "[다이얼로그 제목]", // 제목
+                        "다이얼로그 내용 표시하기", // 내용
+                        leftListener, // 왼쪽 버튼 이벤트
+                        rightListener); // 오른쪽 버튼 이벤트
+                mPopupActivity.show();
+                break;
         }
     }
 
-    public void getBookimageThread(final String barcode) throws InterruptedException {
-        Thread thread = new Thread() {
-            public void run() {
-                try {
-                    URL url = new URL("http://t1.daumcdn.net/book/KOR"+barcode);
+    private View.OnClickListener leftListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "왼쪽버튼 클릭",
+                    Toast.LENGTH_SHORT).show();
+            mPopupActivity.dismiss();
+        }
+    };
 
-                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                    con.setDoInput(true);
-                    con.connect();
-
-                    InputStream inputStream = con.getInputStream();
-                    bitmap = BitmapFactory.decodeStream(inputStream);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-        thread.start();
-        thread.join();
-    }
+    private View.OnClickListener rightListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(), "오른쪽버튼 클릭",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
 
 
 
